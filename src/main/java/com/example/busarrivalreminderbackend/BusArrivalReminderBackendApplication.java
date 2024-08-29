73e77b5dec6e60ec8f8207732b298d12f24eafc4
Seonghun Jeong <tjdgns5506@gmail.com>
2024-08-29 19:34:49 +0900

    chore: 초기 설정 및 unversioned 파일 추가

---
+++ src/main/java/com/example/busarrivalreminderbackend/BusArrivalReminderBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.busarrivalreminderbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusArrivalReminderBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusArrivalReminderBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/busarrivalreminderbackend/BusArrivalReminderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.busarrivalreminderbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusArrivalReminderBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
