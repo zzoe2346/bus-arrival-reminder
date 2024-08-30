33c6a6e825d60fa55bfabab63faa965a92b96533
Seonghun Jeong <tjdgns5506@gmail.com>
2024-08-30 14:57:09 +0900

    feat: `@JsonProperty`적용
    - `@JsonProperty`적용해야 객체와 JSON이 매핑이 정상적으로 된다.

--- src/main/java/com/example/busarrivalreminderbackend/dto/BusArrivalInfoResponse.java
+++ src/main/java/com/example/busarrivalreminderbackend/dto/BusArrivalInfoResponse.java
@@ -1,5 +1,7 @@
 package com.example.busarrivalreminderbackend.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.List;
 
 public class BusArrivalInfoResponse {
@@ -24,32 +26,63 @@ public class BusArrivalInfoResponse {
                 .orElse(-1);
     }
 
+    @JsonProperty("response")
     private Response response;
 
     public static class Response {
+        @JsonProperty("header")
         private Header header;
+
+        @JsonProperty("body")
         private Body body;
     }
 
     public static class Header {
+        @JsonProperty("resultCode")
         private String resultCode;
+
+        @JsonProperty("resultMsg")
+        private String resultMsg;
     }
 
     public static class Body {
+        @JsonProperty("items")
         private Items items;
+
+        @JsonProperty("numOfRows")
+        private int numOfRows;
+
+        @JsonProperty("pageNo")
+        private int pageNo;
+
+        @JsonProperty("totalCount")
+        private int totalCount;
     }
 
     public static class Items {
+        @JsonProperty("item")
         private List<Item> item;
     }
 
     public static class Item {
+        @JsonProperty("arrprevstationcnt")
         private int arrprevstationcnt;
+
+        @JsonProperty("arrtime")
         private int arrtime;
+
+        @JsonProperty("nodeid")
         private String nodeid;
+
+        @JsonProperty("nodenm")
         private String nodenm;
+
+        @JsonProperty("routeid")
         private String routeid;
+
+        @JsonProperty("routeno")
         private String routeno;
+
 
         public int getArrprevstationcnt() {
             return arrprevstationcnt;
