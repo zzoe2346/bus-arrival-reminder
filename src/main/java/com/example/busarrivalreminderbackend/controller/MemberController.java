792d265052220a68cd6826c494ff99d527349e01
Seonghun Jeong <tjdgns5506@gmail.com>
2024-09-13 19:48:47 +0900

    feat: MemberController에 checkId api 추가
    - 회원가입할때 Id 중복 확인하는 api

--- src/main/java/com/example/busarrivalreminderbackend/controller/MemberController.java
+++ src/main/java/com/example/busarrivalreminderbackend/controller/MemberController.java
@@ -1,13 +1,20 @@
 package com.example.busarrivalreminderbackend.controller;
 
 import com.example.busarrivalreminderbackend.dto.SignUpRequest;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public class MemberController {
 
-    @PostMapping("/api/signup")
+    @PostMapping("/api/members/signup")
     public void signUp(@RequestBody SignUpRequest request) {
+
+    }
+
+    @GetMapping("/api/members/check-id")
+    public void checkId(@RequestParam String id) {
 
     }
 
